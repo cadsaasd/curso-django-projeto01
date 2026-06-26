b038bc65e2262506a2bb4c78f486182b79bda95e
caua <cauasilva3643@gmail.com>
2026-06-25 22:19:36 -0300

    Implementa links dinâmicos nos templates usando a tag url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<id>/', views.recipe),
+    path('', views.home, name='home'),
+    path('recipes/<id>/', views.recipe, name='recipe'),
 ]
